7769c4c655d4f75d8ca2cfd7239f4c5b38877be7
Lixa2604 <corralesfrancoyalixa@gmail.com>
2026-07-24 09:17:50 -0400

    extraer correo

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
 // DOM
 const correo = document.getElementById("correo")
 
-console.log(correo)
+const formulario = document.getElementById("formulario")
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    console.log(correo.value)
+})
