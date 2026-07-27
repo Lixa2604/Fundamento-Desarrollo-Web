b437a54df9bea9a084818f929a1ee99c8a58ad4b
Lixa2604 <corralesfrancoyalixa@gmail.com>
2026-07-27 11:17:18 -0400

    async function iniciarSesion

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 // DOM
 const correo = document.getElementById("correo")
+const password = document.getElementById("contrasenia")
 
 const formulario = document.getElementById("formulario")
 
@@ -7,4 +8,46 @@ formulario.addEventListener("submit", (event) => {
     event.preventDefault()
 
     console.log(correo.value)
+    
 })
+
+async function obtenercelulares() {
+    const url = "http://localhost:8000/celulares";
+
+    try {
+        // Escribir codigo
+        const respuesta = await fetch(url);
+
+        if (!respuesta.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const resultado = await respuesta.json();
+        console.log(resultado);
+    }   catch (error) {
+        console.error(error.message);
+    }
+}
+
+async function iniciarSesion(correo, password) {
+    const url = "http://localhost:8000/iniciar-sesion"
+
+    try {
+        // Escribir codigo
+        const respuesta = await fetch(url, {
+            method: "POST"
+            body: JSON.stringify({ correo: correo, password: password})
+        });
+
+        if (!respuesta.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const resultado = await respuesta.json();
+        console.log(resultado);
+    }   catch (error) {
+        console.error(error.message);
+    }
+}
+
+ obtenercelulares()
